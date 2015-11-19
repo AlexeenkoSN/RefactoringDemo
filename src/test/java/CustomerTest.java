fca4ae97d0f6085d3039468307f2195c1f1f4255
alexeenkosn <alexeenkosn@gmail.com>
2015-11-19 15:40:26 +0300

    working files

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -1,5 +1,19 @@
-/**
- * Created by root on 19.11.2015.
- */
+import com.scrumtrek.simplestore.Customer;
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import com.scrumtrek.simplestore.Rental;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CustomerTest {
+
+    @Test
+    public void shouldStatementContainRightPriceWhenRegularMoviePassed(){
+        Customer testee = new Customer("");
+        Movie movie = new Movie("", PriceCodes.Regular);
+        Rental rental = new Rental(movie, 3);
+        testee.addRental(rental);
+
+        Assert.assertTrue(testee.Statement().contains("3.5"));
+    }
 }
--- src/test/java/RentalTest.java
+++ src/test/java/RentalTest.java
@@ -1,5 +1,28 @@
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.Rental;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.stub;
+
 /**
  * Created by root on 19.11.2015.
  */
 public class RentalTest {
+    @Test
+    public void shouldRentalContainRightRentalDaysWhenCreated(){
+        Movie dummy = mock(Movie.class);
+        Rental rental = new Rental(dummy, 4);
+
+        Assert.assertEquals(4, rental.getDaysRented());
+    }
+
+    @Test
+    public void shouldRentalContainRightMovieWhenCreated(){
+        Movie dummy = mock(Movie.class);
+        Rental rental = new Rental(dummy, 4);
+
+        Assert.assertEquals(dummy, rental.getMovie());
+    }
 }
