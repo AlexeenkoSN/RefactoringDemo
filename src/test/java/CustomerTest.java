71632581ff1a70b9d97397113c72102b0a48a0fe
alexeenkosn <alexeenkosn@gmail.com>
2015-11-20 15:15:50 +0300

    review

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -8,11 +8,47 @@ import org.junit.Test;
 public class CustomerTest {
 
     @Test
-    public void shouldStatementContainRightPriceWhenRegularMoviePassed(){
+    public void shouldStatementContainRightPriceWhenRegularMoviePassedAndDaysLessOrEqualThanTwo() {
+        Customer testee = new Customer("");
+        Movie movie = new Movie("", PriceCodes.Regular);
+        Rental rental = new Rental(movie, 1);
+        testee.addRental(rental);
+        Assert.assertTrue("Check price correctness for regular movie", testee.buildStatement().contains("2"));
+    }
+
+    @Test
+    public void shouldStatementContainRightPriceWhenRegularMoviePassedDaysMoreThanTwo(){
         Customer testee = new Customer("");
         Movie movie = new Movie("", PriceCodes.Regular);
         Rental rental = new Rental(movie, 3);
         testee.addRental(rental);
         Assert.assertTrue("Check price correctness for regular movie", testee.buildStatement().contains("3.5"));
+    }
+
+    @Test
+    public void shouldStatementContainRightPriceWhenNewReleaseMoviePassed(){
+        Customer testee = new Customer("");
+        Movie movie = new Movie("", PriceCodes.NewRelease);
+        Rental rental = new Rental(movie, 3);
+        testee.addRental(rental);
+        Assert.assertTrue("Check price correctness for new release movie", testee.buildStatement().contains("9"));
+    }
+
+    @Test
+    public void shouldStatementContainRightPriceWhenChildrenMoviePassedWhenDaysLassOrEqualThanThree(){
+        Customer testee = new Customer("");
+        Movie movie = new Movie("", PriceCodes.Childrens);
+        Rental rental = new Rental(movie, 1);
+        testee.addRental(rental);
+        Assert.assertTrue("Check price correctness for children's movie", testee.buildStatement().contains("1.5"));
+    }
+
+    @Test
+    public void shouldStatementContainRightPriceWhenChildrenMoviePassedWhenDaysMoreThanThree(){
+        Customer testee = new Customer("");
+        Movie movie = new Movie("", PriceCodes.Childrens);
+        Rental rental = new Rental(movie, 5);
+        testee.addRental(rental);
+        Assert.assertTrue("Check price correctness for children's movie", testee.buildStatement().contains("3"));
     }
 }
