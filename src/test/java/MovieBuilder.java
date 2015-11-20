0e5026ef77d99c653bd8a13317705d369bb86747
alexeenkosn <alexeenkosn@gmail.com>
2015-11-20 11:34:55 +0300

    pom

--- src/test/java/MovieBuilder.java
+++ src/test/java/MovieBuilder.java
@@ -1,5 +1,30 @@
-/**
- * Created by root on 19.11.2015.
- */
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+
 public class MovieBuilder {
+
+    private String title;
+    private PriceCodes code;
+
+    private MovieBuilder() {
+    }
+
+    public static MovieBuilder create()
+    {
+        return new MovieBuilder();
+    }
+
+    public MovieBuilder withTitle(String title){
+        this.title = title;
+        return this;
+    }
+
+    public MovieBuilder withPriceCode(PriceCodes priceCode){
+        this.code = priceCode;
+        return this;
+    }
+
+    public Movie build(){
+        return new Movie(title, code);
+    }
 }
