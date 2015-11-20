66208345565d9c649ddb6caa57af185a81cb6a10
alexeenkosn <alexeenkosn@gmail.com>
2015-11-20 15:05:54 +0300

    review

--- src/test/java/MovieTest.java
+++ src/test/java/MovieTest.java
@@ -1,5 +1,17 @@
-/**
- * Created by root on 19.11.2015.
- */
+import com.scrumtrek.simplestore.Movie;
+import com.scrumtrek.simplestore.PriceCodes;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MovieTest {
+    @Test
+    public void shouldPriceAndTitleBeFilledWhenConstructed()
+    {
+        String expectedTitle = "StarWars";
+        PriceCodes expectedPriceCode = PriceCodes.Regular;
+        Movie testee = new Movie(expectedTitle, expectedPriceCode);
+
+        Assert.assertEquals("Check that title filled", expectedTitle, testee.getTitle());
+        Assert.assertEquals("Check that price code filled", expectedPriceCode, testee.getPriceCode());
+    }
 }
